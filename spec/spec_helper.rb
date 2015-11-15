dba6638b30a03e75906b3d2f14213e77c75e6fe7
Akiko Takano <akiko.pusu@gmail.com>
2015-11-16 01:25:37 +0900

    Change to use Redmine's fixtures.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'simplecov'
 SimpleCov.start 'rails'
 
 RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/test/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
