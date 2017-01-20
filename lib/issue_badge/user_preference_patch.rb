1917920c11e978465421e289f325b44aea1c53f6
Akiko Takano <akiko.pusu@gmail.com>
2017-01-21 00:42:20 +0900

    Remove blank line.

--- lib/issue_badge/user_preference_patch.rb
+++ lib/issue_badge/user_preference_patch.rb
@@ -1,6 +1,5 @@
 module IssueBadge
   module UserPreferencePatch
-
     def self.included(base)
       base.class_eval do
         unloadable # Send unloadable so it will not be unloaded in development
