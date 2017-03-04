db0214e7683919eecc5cf7c33481c1091cce7be6
Akiko Takano <akiko.pusu@gmail.com>
2017-03-05 00:35:46 +0900

    :sparkles: Update spect for #36, to check in case user is required password change.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require File.expand_path('../../../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'simplecov'
@@ -6,7 +7,7 @@ SimpleCov.start 'rails'
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/test/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
   FactoryGirl.definition_file_paths = [File.expand_path('../factories', __FILE__)]
