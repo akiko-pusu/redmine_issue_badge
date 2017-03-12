0e822e0c557220530278a4a610f9872965a3b2c6
Akiko Takano <akiko.pusu@gmail.com>
2017-03-12 09:31:05 +0900

    Update init.rb and set default polling option is false.

--- init.rb
+++ init.rb
@@ -16,13 +16,14 @@ Redmine::Plugin.register :redmine_issue_badge do
   name 'Redmine Issue Badge plugin'
   author 'Akiko Takano'
   description 'Plugin to show the number of assigned issues with badge on top menu.'
-  version '0.0.4.1'
+  version '0.0.5-dev'
   url 'https://github.com/akiko-pusu/redmine_issue_badge'
   author_url 'http://twitter.com/akiko_pusu'
   requires_redmine version_or_higher: '3.3.0'
 
   settings partial: 'settings/redmine_issue_badge',
            default: {
-             'activate_for_all_users' => 'false'
+             'activate_for_all_users' => 'false',
+             'enabled_polling' => false
            }
 end
