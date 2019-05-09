849950827ae4f6cfde598c41fc29521f8a3f0581
Akiko Takano <akiko.pusu@gmail.com>
2019-05-10 00:28:58 +0900

    Update version to 0.0.7. Add mention that this version does not support Redmine4 completely.

--- db/migrate/201703120951_add_show_assigned_to_group_to_issue_badge_user_settings.rb
+++ db/migrate/201703120951_add_show_assigned_to_group_to_issue_badge_user_settings.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class AddShowAssignedToGroupToIssueBadgeUserSettings < ActiveRecord::Migration
   def self.up
     add_column :issue_badge_user_settings, :show_assigned_to_group, :boolean
@@ -8,3 +9,7 @@ class AddShowAssignedToGroupToIssueBadgeUserSettings < ActiveRecord::Migration
     remove_column :issue_badge_user_settings, :show_assigned_to_group
   end
 end
+
+# For Redmine4.x user, especially under clean install:
+# Please change ActiveRecord::Migration to ActiveRecord::Migration[4.2] in case
+# migration failed.
--- db/migrate/201811090021_add_badge_order_to_issue_badge_user_settings.rb
+++ db/migrate/201811090021_add_badge_order_to_issue_badge_user_settings.rb
@@ -9,3 +9,7 @@ class AddBadgeOrderToIssueBadgeUserSettings < ActiveRecord::Migration
     remove_column :issue_badge_user_settings, :badge_order
   end
 end
+
+# For Redmine4.x user, especially under clean install:
+# Please change ActiveRecord::Migration to ActiveRecord::Migration[4.2] in case
+# migration failed.
