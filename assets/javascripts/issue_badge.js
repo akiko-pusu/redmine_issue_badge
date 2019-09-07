678a7d590a79338202c50922cfdc7e4b9045f4c1
Akiko Takano <akiko.pusu@gmail.com>
2019-09-07 19:24:51 +0900

    Code Refactoring. Replace jQuery and so on. Remove some JavaScript code from erb.

--- assets/javascripts/issue_badge.js
+++ assets/javascripts/issue_badge.js
@@ -1,7 +1,7 @@
 // call issue_badge
 /* eslint-disable no-unused-vars */
 /* eslint-env jquery */
-const loadBadge = (url) => {
+const loadBadge = (url, optionPollUrl) => {
   let request = new window.XMLHttpRequest()
   request.open('GET', url, true)
 
@@ -9,7 +9,12 @@ const loadBadge = (url) => {
     if (request.status >= 200 && request.status < 400) {
       let html = request.response
       if (html.length > 0) {
-        $('#loggedas').after(html)
+        console.log('load badge')
+        document.getElementById('loggedas').insertAdjacentHTML('afterend', html)
+        if (optionPollUrl) {
+          pollBadgeCount(optionPollUrl)
+        }
+        changeBadgeLocation()
       }
     } else {
       console.log('[IssueBadge] LoadBadge Response Error. Status: ' + request.status)
@@ -46,17 +51,57 @@ const displayBadgeContents = (url) => {
 const changeBadgeLocation = () => {
   const issueBadgeElement = document.getElementById('issue_badge')
   if (window.matchMedia('(max-width: 899px)').matches) {
-    $('#quick-search').prepend($('#issue_badge'))
+    const quickSearch = document.getElementById('quick-search')
+
+    quickSearch.insertBefore(issueBadgeElement, quickSearch.firstChild)
   } else {
-    $('#loggedas').after($('#issue_badge'))
+    document.getElementById('loggedas').insertAdjacentElement('afterend', issueBadgeElement)
   }
 }
 
-document.addEventListener('click', function (event) {
-  if (!$(event.target).is('#issue_badge_contents')) {
-    $('#issue_badge_contents').hide()
-    $('#issue_badge_contents').remove()
+document.addEventListener('click', (event) => {
+  const badgeContents = document.getElementById('issue_badge_contents')
+
+  if (badgeContents && event.target !== badgeContents) {
+    badgeContents.remove()
   }
 })
+
+window.onresize = () => {
+  changeBadgeLocation()
+}
+
+// Polling setting
+const pollBadgeCount = (pollingUrl) => {
+  const poll = (pollingUrl) => {
+    let status = document.getElementById('issue_badge_number')
+    let request = new window.XMLHttpRequest()
+
+    request.open('GET', pollingUrl, true)
+
+    request.onload = () => {
+      if (request.status >= 200 && request.status < 400) {
+        let data = request.response
+        if (typeof data.all_issues_count !== 'undefined' && data.status === true) {
+          status.textContent = data.all_issues_count
+        } else {
+          console.log("[IssueBadge] Error. Can't parse polling data.")
+          status.textContent = '?'
+          clearInterval(pollInterval)
+        }
+      } else {
+        console.log("[IssueBadge] Error. Can't receive polling data.")
+        clearInterval(pollInterval)
+      }
+    }
+
+    request.onerror = () => {
+      console.log("[IssueBadge] LoadBadge Request Error. Can't get badge data.")
+    }
+    request.responseType = 'json'
+    request.send()
+  }
+  const pollInterval = setInterval(poll, 60000, pollingUrl)
+}
 
 /* eslint-enable no-unused-vars */
