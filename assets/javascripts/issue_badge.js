a3203f5cd621e87417e0291e7186d5b31902a641
Akiko Takano <akiko.pusu@gmail.com>
2019-09-09 13:11:54 +0900

    Split JavaScript files not to load unused function.

--- assets/javascripts/issue_badge.js
+++ assets/javascripts/issue_badge.js
@@ -71,7 +71,6 @@ const changeBadgeLocation = () => {
 
 window.onresize = () => {
   changeBadgeLocation()
-  changePrefLocation()
 }
 
 // Common method to send request and return response text
@@ -98,36 +97,6 @@ const baseRequest = (url, type) => {
     }
     request.send()
   })
-}
-
-/* --------- My preference Page ----------- */
-
-const changePrefLocation = () => {
-  const element = document.getElementById('pref_issue_badge')
-  const target = document.querySelector('div.splitcontentright')
-
-  if (window.matchMedia('(max-width: 899px)').matches) {
-    const mobileTarget = target.querySelector('p.mobile-show')
-    target.insertBefore(element, mobileTarget)
-  } else {
-    target.appendChild(element)
-  }
-}
-
-const showOptionalBadgeSetting = () => {
-  toggleItem('show_assigned_to_group')
-  toggleItem('badge_order')
-}
-
-const toggleItem = (id) => {
-  const element = document.getElementById(id)
-
-  let style = element.style.display
-  element.style.display = (style === 'block') ? 'none' : 'block'
-}
-
-window.onload = () => {
-  changePrefLocation()
 }
 
 /* eslint-enable no-unused-vars */
--- lib/issue_badge/application_hooks.rb
+++ lib/issue_badge/application_hooks.rb
@@ -4,12 +4,16 @@ module IssueBadge
   class ApplicationHooks < Redmine::Hook::ViewListener
     include ApplicationHelper
 
-    def view_layouts_base_html_head(_context = {})
+    def view_layouts_base_html_head(context = {})
+      controller = context[:controller]
       o = ''
       if User.current.logged?
         global_enabled = Setting.plugin_redmine_issue_badge['activate_for_all_users'] == 'true'
         issue_badge = IssueBadgeUserSetting.find_or_create_by_user_id(User.current)
         o = stylesheet_link_tag('style', plugin: 'redmine_issue_badge')
+
+        o << javascript_include_tag('issue_badge_my_preference', plugin: 'redmine_issue_badge') if my_preference_page?(controller)
+
         if issue_badge.try(:enabled?) || global_enabled
           o << javascript_include_tag('issue_badge', plugin: 'redmine_issue_badge')
           o << "\n".html_safe + javascript_tag(build_load_badge_script)
@@ -26,6 +30,12 @@ module IssueBadge
       return "loadBadge('#{escape_javascript badge_url}');" unless polling_option
 
       "loadBadge('#{escape_javascript badge_url}', '#{issue_badge_issues_count_path}');"
+    end
+
+    def my_preference_page?(controller)
+      return false unless controller&.class&.name == 'MyController'
+
+      controller.action_name == 'account'
     end
   end
 end
